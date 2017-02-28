959e7a22b6522399e7359d291050627b769118ff
Luis Padilla <luis.padilla.93@gmail.com>
2017-02-28 16:47:08 -0600

    Scripts

--- app.js
+++ app.js
@@ -26,3 +26,31 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+
+
+
+
+
+'use strict';
+
+const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+const fs = require('fs');
+
+const visual_recognition = new VisualRecognitionV3({
+  api_key: '3735956cf7c2cb2d6dad568e98a3388c845a3adf',
+  version_date: '2016-05-19'
+});
+
+const params = {
+  // must be a .zip file containing images
+  images_file: fs.createReadStream('./resources/car.png')
+};
+
+visual_recognition.classify(params, function(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.stringify(res, null, 2));
+  }
+});
