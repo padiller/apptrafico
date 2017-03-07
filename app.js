67797a045056a1ef61dbc01f9c76527d8c311ee8
Luis Padilla <luis.padilla.93@gmail.com>
2017-03-07 16:27:52 -0600

    Probando Solo Require

--- app.js
+++ app.js
@@ -31,9 +31,3 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 
 
 var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
-var fs = require('fs');
-
-var visual_recognition = new VisualRecognitionV3({
-  api_key: '3735956cf7c2cb2d6dad568e98a3388c845a3adf',
-  version_date: '2016-05-19'
-});
